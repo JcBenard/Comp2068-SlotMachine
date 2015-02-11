43397492c282dfd1eb5e05f245627670f945df91
JcBenard <niteburn@hotmail.com>
2015-02-11 11:57:26 +0900

    Added event listeners for the buttons

--- COMP2068-SlotMachine/COMP2068-SlotMachine/Scripts/game.js
+++ COMP2068-SlotMachine/COMP2068-SlotMachine/Scripts/game.js
@@ -10,6 +10,7 @@ var bet1Button;
 var bet5Button;
 var bet10Button;
 var resetBetButton;
+var bettingAmount;
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -25,16 +26,60 @@ function gameLoop() {
     stage.update();
 }
 
-function spinButtonOut() {
-    spinButton.alpha = 1.0;
+function buttonOut(button) {
+    switch (button) {
+        case "spinButton":
+            spinButton.alpha = 1.0;
+            break;
+        case "resetButton":
+            resetButton.alpha = 1.0;
+            break;
+        case "bet1Button":
+            bet1Button.alpha = 1.0;
+            break;
+        case "bet5Button":
+            bet5Button.alpha = 1.0;
+            break;
+        case "bet10Button":
+            bet10Button.alpha = 1.0;
+            break;
+        case "resetBetButton":
+            resetBetButton.alpha = 1.0;
+            break;
+    }
 }
 
-function spinButtonOver() {
-    spinButton.alpha = 0.7;
+function buttonOver(button) {
+    switch (button) {
+        case "spinButton":
+            spinButton.alpha = 0.8;
+            break;
+        case "resetButton":
+            resetButton.alpha = 0.8;
+            break;
+        case "bet1Button":
+            bet1Button.alpha = 0.8;
+            break;
+        case "bet5Button":
+            bet5Button.alpha = 0.8;
+            break;
+        case "bet10Button":
+            bet10Button.alpha = 0.8;
+            break;
+        case "resetBetButton":
+            resetBetButton.alpha = 0.8;
+            break;
+    }
 }
 
 function spinReels() {
     //add the code to spin reels
+}
+
+function resetGame() {
+}
+
+function betting(ammount) {
 }
 
 function createUi() {
@@ -45,43 +90,95 @@ function createUi() {
     //instatite the spin button
     spinButton = new createjs.Bitmap("assets/images/spin.png");
     spinButton.x = 456;
-    spinButton.y = 535;
+    spinButton.y = 537;
     game.addChild(spinButton);
+
+    spinButton.addEventListener("click", spinReels);
+    spinButton.addEventListener("mouseover", function () {
+        buttonOver("spinButton");
+    });
+    spinButton.addEventListener("mouseout", function () {
+        buttonOut("spinButton");
+    });
 
     //instatite the reset button
     resetButton = new createjs.Bitmap("assets/images/reset.png");
     resetButton.x = 61;
-    resetButton.y = 539;
+    resetButton.y = 537;
     game.addChild(resetButton);
+
+    resetButton.addEventListener("click", resetGame);
+    resetButton.addEventListener("mouseover", function () {
+        buttonOver("resetButton");
+    });
+    resetButton.addEventListener("mouseout", function () {
+        buttonOut("resetButton");
+    });
 
     //instatite the bet 1 button
     bet1Button = new createjs.Bitmap("assets/images/bet1.png");
-    bet1Button.x = 143;
-    bet1Button.y = 538;
+    bet1Button.x = 148;
+    bet1Button.y = 537;
     game.addChild(bet1Button);
 
+    bet1Button.addEventListener("click", function () {
+        betting(1);
+    });
+    bet1Button.addEventListener("mouseover", function () {
+        buttonOver("bet1Button");
+    });
+    bet1Button.addEventListener("mouseout", function () {
+        buttonOut("bet1Button");
+    });
+
     //instatite the bet 5 button
-    bet5Button = new createjs.Bitmap("assets/images/spin.png");
-    bet5Button.x = 217;
+    bet5Button = new createjs.Bitmap("assets/images/bet5.png");
+    bet5Button.x = 222;
     bet5Button.y = 537;
     game.addChild(bet5Button);
 
+    bet5Button.addEventListener("click", function () {
+        betting(5);
+    });
+    bet5Button.addEventListener("mouseover", function () {
+        buttonOver("bet5Button");
+    });
+    bet5Button.addEventListener("mouseout", function () {
+        buttonOut("bet5Button");
+    });
+
     //instatite the bet 10 button
-    bet10Button = new createjs.Bitmap("assets/images/spin.png");
+    bet10Button = new createjs.Bitmap("assets/images/bet10.png");
     348, 537;
-    bet10Button.x = 288;
-    bet10Button.y = 535;
+    bet10Button.x = 293;
+    bet10Button.y = 537;
     game.addChild(bet10Button);
 
-    //instatite the bet 10 button
-    resetBetButton = new createjs.Bitmap("assets/images/spin.png");
-    resetBetButton.x = 361;
-    resetBetButton.y = 535;
+    bet10Button.addEventListener("click", function () {
+        betting(10);
+    });
+    bet10Button.addEventListener("mouseover", function () {
+        buttonOver("bet10Button");
+    });
+    bet10Button.addEventListener("mouseout", function () {
+        buttonOut("bet10Button");
+    });
+
+    //instatite the reset bet button
+    resetBetButton = new createjs.Bitmap("assets/images/resetBet.png");
+    resetBetButton.x = 366;
+    resetBetButton.y = 537;
     game.addChild(resetBetButton);
 
-    spinButton.addEventListener("click", spinReels);
-    spinButton.addEventListener("mouseover", spinButtonOver);
-    spinButton.addEventListener("mouseout", spinButtonOut);
+    resetBetButton.addEventListener("click", function () {
+        betting(null);
+    });
+    resetBetButton.addEventListener("mouseover", function () {
+        buttonOver("resetBetButton");
+    });
+    resetBetButton.addEventListener("mouseout", function () {
+        buttonOut("resetBetButton");
+    });
 }
 
 // Our Game Kicks off in here
@@ -90,7 +187,7 @@ function main() {
     game = new createjs.Container();
 
     //get the slots ui
-    createUi;
+    createUi();
 
     stage.addChild(game);
 }
--- COMP2068-SlotMachine/COMP2068-SlotMachine/Scripts/game.ts
+++ COMP2068-SlotMachine/COMP2068-SlotMachine/Scripts/game.ts
@@ -10,6 +10,7 @@ var bet1Button: createjs.Bitmap;
 var bet5Button: createjs.Bitmap;
 var bet10Button: createjs.Bitmap;
 var resetBetButton: createjs.Bitmap;
+var bettingAmount: number;
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -25,17 +26,64 @@ function gameLoop() {
     stage.update();
 }
 
-function spinButtonOut() {
-    spinButton.alpha = 1.0;
+function buttonOut(button: string) {
+    switch (button) {
+        case "spinButton":
+            spinButton.alpha = 1.0;
+            break;
+        case "resetButton":
+            resetButton.alpha = 1.0;
+            break;
+        case "bet1Button":
+            bet1Button.alpha = 1.0;
+            break;
+        case "bet5Button":
+            bet5Button.alpha = 1.0;
+            break;
+        case "bet10Button":
+            bet10Button.alpha = 1.0;
+            break;
+        case "resetBetButton":
+            resetBetButton.alpha = 1.0;
+            break;
+    }
 }
 
-function spinButtonOver() {
-    spinButton.alpha = 0.7;
+function buttonOver(button: string) {
+
+    switch (button) {
+        case "spinButton":
+            spinButton.alpha = 0.8;
+            break;
+        case "resetButton":
+            resetButton.alpha = 0.8;
+            break;
+        case "bet1Button":
+            bet1Button.alpha = 0.8;
+            break;
+        case "bet5Button":
+            bet5Button.alpha = 0.8;
+            break;
+        case "bet10Button":
+            bet10Button.alpha = 0.8;
+            break;
+        case "resetBetButton":
+            resetBetButton.alpha = 0.8;
+            break;
+    }
 }
 
 function spinReels() {
     //add the code to spin reels
     
+}
+
+function resetGame() {
+
+}
+
+function betting(ammount: number) {
+
 }
 
 function createUi(): void {
@@ -47,42 +95,62 @@ function createUi(): void {
     //instatite the spin button
     spinButton = new createjs.Bitmap("assets/images/spin.png");
     spinButton.x = 456 // set it to the x coord i got
-    spinButton.y = 535 // set it to the y coord i got
+    spinButton.y = 537 // set it to the y coord i got
     game.addChild(spinButton);
+
+    spinButton.addEventListener("click", spinReels);
+    spinButton.addEventListener("mouseover", function () { buttonOver("spinButton"); });
+    spinButton.addEventListener("mouseout", function () { buttonOut("spinButton"); });
 
     //instatite the reset button
     resetButton = new createjs.Bitmap("assets/images/reset.png");
     resetButton.x = 61 // set it to the x coord i got
-    resetButton.y = 539 // set it to the y coord i got
+    resetButton.y = 537 // set it to the y coord i got
     game.addChild(resetButton);
+
+    resetButton.addEventListener("click", resetGame);
+    resetButton.addEventListener("mouseover", function () { buttonOver("resetButton"); });
+    resetButton.addEventListener("mouseout", function () { buttonOut("resetButton"); });
 
     //instatite the bet 1 button
     bet1Button = new createjs.Bitmap("assets/images/bet1.png");
-    bet1Button.x = 143 // set it to the x coord i got
-    bet1Button.y = 538 // set it to the y coord i got
+    bet1Button.x = 148 // set it to the x coord i got
+    bet1Button.y = 537 // set it to the y coord i got
     game.addChild(bet1Button);
 
+    bet1Button.addEventListener("click", function () { betting(1); });
+    bet1Button.addEventListener("mouseover", function () { buttonOver("bet1Button"); });
+    bet1Button.addEventListener("mouseout", function () { buttonOut("bet1Button"); });
+
     //instatite the bet 5 button
-    bet5Button = new createjs.Bitmap("assets/images/spin.png");
-    bet5Button.x = 217 // set it to the x coord i got
+    bet5Button = new createjs.Bitmap("assets/images/bet5.png");
+    bet5Button.x = 222 // set it to the x coord i got
     bet5Button.y = 537 // set it to the y coord i got
     game.addChild(bet5Button);
 
+    bet5Button.addEventListener("click", function () { betting(5); });
+    bet5Button.addEventListener("mouseover", function () { buttonOver("bet5Button"); });
+    bet5Button.addEventListener("mouseout", function () { buttonOut("bet5Button"); });
+
     //instatite the bet 10 button
-    bet10Button = new createjs.Bitmap("assets/images/spin.png"); 348, 537
-    bet10Button.x = 288 // set it to the x coord i got
-    bet10Button.y = 535 // set it to the y coord i got
+    bet10Button = new createjs.Bitmap("assets/images/bet10.png"); 348, 537
+    bet10Button.x = 293 // set it to the x coord i got
+    bet10Button.y = 537 // set it to the y coord i got
     game.addChild(bet10Button);
 
-    //instatite the bet 10 button
-    resetBetButton = new createjs.Bitmap("assets/images/spin.png");
-    resetBetButton.x = 361 // set it to the x coord i got
-    resetBetButton.y = 535 // set it to the y coord i got
+    bet10Button.addEventListener("click", function () { betting(10); });
+    bet10Button.addEventListener("mouseover", function () { buttonOver("bet10Button"); });
+    bet10Button.addEventListener("mouseout", function () { buttonOut("bet10Button"); });
+
+    //instatite the reset bet button
+    resetBetButton = new createjs.Bitmap("assets/images/resetBet.png");
+    resetBetButton.x = 366 // set it to the x coord i got
+    resetBetButton.y = 537 // set it to the y coord i got
     game.addChild(resetBetButton);
 
-    spinButton.addEventListener("click", spinReels);
-    spinButton.addEventListener("mouseover", spinButtonOver);
-    spinButton.addEventListener("mouseout", spinButtonOut);
+    resetBetButton.addEventListener("click", function () { betting(null); });
+    resetBetButton.addEventListener("mouseover", function () { buttonOver("resetBetButton"); });
+    resetBetButton.addEventListener("mouseout", function () { buttonOut("resetBetButton"); });
 
 }
 
@@ -94,7 +162,7 @@ function main() {
     game = new createjs.Container();
 
     //get the slots ui
-    createUi;
+    createUi();
 
     stage.addChild(game);    
 }
