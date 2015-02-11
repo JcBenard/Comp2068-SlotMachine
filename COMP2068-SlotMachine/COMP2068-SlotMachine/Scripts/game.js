541d49d53df548835d99af3bf0e76f2013e7cd2b
JcBenard <niteburn@hotmail.com>
2015-02-11 11:15:28 +0900

    Added buttons

--- COMP2068-SlotMachine/COMP2068-SlotMachine/Scripts/game.js
+++ COMP2068-SlotMachine/COMP2068-SlotMachine/Scripts/game.js
@@ -1,15 +1,15 @@
 ﻿var canvas;
-var stage: createjs.Stage;
+var stage;
 
-// Game Objects 
-var game: createjs.Container;
-var background: createjs.Bitmap;
-var spinButton: createjs.Bitmap;
-var resetButton: createjs.Bitmap;
-var bet1Button: createjs.Bitmap;
-var bet5Button: createjs.Bitmap;
-var bet10Button: createjs.Bitmap;
-var resetBetButton: createjs.Bitmap;
+// Game Objects
+var game;
+var background;
+var spinButton;
+var resetButton;
+var bet1Button;
+var bet5Button;
+var bet10Button;
+var resetBetButton;
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -35,58 +35,54 @@ function spinButtonOver() {
 
 function spinReels() {
     //add the code to spin reels
-    
 }
 
-function createUi(): void {
-
+function createUi() {
     //instantate the background
     background = new createjs.Bitmap("assets/images/background-slot.png");
     game.addChild(background);
 
     //instatite the spin button
     spinButton = new createjs.Bitmap("assets/images/spin.png");
-    spinButton.x = 456 // set it to the x coord i got
-    spinButton.y = 535 // set it to the y coord i got
+    spinButton.x = 456;
+    spinButton.y = 535;
     game.addChild(spinButton);
 
     //instatite the reset button
     resetButton = new createjs.Bitmap("assets/images/reset.png");
-    resetButton.x = 61 // set it to the x coord i got
-    resetButton.y = 539 // set it to the y coord i got
+    resetButton.x = 61;
+    resetButton.y = 539;
     game.addChild(resetButton);
 
     //instatite the bet 1 button
     bet1Button = new createjs.Bitmap("assets/images/bet1.png");
-    bet1Button.x = 143 // set it to the x coord i got
-    bet1Button.y = 538 // set it to the y coord i got
+    bet1Button.x = 143;
+    bet1Button.y = 538;
     game.addChild(bet1Button);
 
     //instatite the bet 5 button
     bet5Button = new createjs.Bitmap("assets/images/spin.png");
-    bet5Button.x = 217 // set it to the x coord i got
-    bet5Button.y = 537 // set it to the y coord i got
+    bet5Button.x = 217;
+    bet5Button.y = 537;
     game.addChild(bet5Button);
 
     //instatite the bet 10 button
-    bet10Button = new createjs.Bitmap("assets/images/spin.png"); 348, 537
-    bet10Button.x = 288 // set it to the x coord i got
-    bet10Button.y = 535 // set it to the y coord i got
+    bet10Button = new createjs.Bitmap("assets/images/spin.png");
+    348, 537;
+    bet10Button.x = 288;
+    bet10Button.y = 535;
     game.addChild(bet10Button);
 
     //instatite the bet 10 button
     resetBetButton = new createjs.Bitmap("assets/images/spin.png");
-    resetBetButton.x = 361 // set it to the x coord i got
-    resetBetButton.y = 535 // set it to the y coord i got
+    resetBetButton.x = 361;
+    resetBetButton.y = 535;
     game.addChild(resetBetButton);
 
     spinButton.addEventListener("click", spinReels);
     spinButton.addEventListener("mouseover", spinButtonOver);
     spinButton.addEventListener("mouseout", spinButtonOut);
-
 }
-
-
 
 // Our Game Kicks off in here
 function main() {
@@ -96,5 +92,6 @@ function main() {
     //get the slots ui
     createUi;
 
-    stage.addChild(game);    
+    stage.addChild(game);
 }
+//# sourceMappingURL=game.js.map
